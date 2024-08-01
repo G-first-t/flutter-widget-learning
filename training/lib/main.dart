26baec2b9c69dee9b2c55e3d9109aa2825a7282b
Gregory kimbira <gregorykimbira7@gmail.com>
2024-08-01 12:35:03 +0300

    working carousel slider

--- training/lib/main.dart
+++ training/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
@@ -16,24 +19,84 @@ void main()=>runApp(MaterialApp(
     Widget build(BuildContext context){
       return Scaffold( 
         appBar:AppBar( 
-          title:const Text("this is my application",
+          title:const Text("Carousel Slider",
           style:TextStyle(
             color:Colors.white,
             fontWeight:FontWeight.bold
             )),
+            centerTitle: true,
             backgroundColor: const Color.fromARGB(255, 2, 20, 51),),
-             body:Container(
-              alignment:Alignment.topCenter,
-              height:200,
-              width:double.infinity,
-              decoration: BoxDecoration(
-                 border:Border.all(
-                  color:Colors.black12,width:5
-                 )
-              ),
-              child:const Text("cool app"),
+             body:ListView( 
+              children:[
+                CarouselSlider(items: [
+                  Container(
+                    margin:const EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(8.0),
+                      image: const DecorationImage(
+                        image:NetworkImage('https://i.pinimg.com/474x/3f/cd/17/3fcd1785622d5eea86a236d9ad795fba.jpg'),
+                        fit: BoxFit.cover
+                      )
+                    )),
+                    Container(
+                      margin:const EdgeInsets.all(2),
+                      decoration:BoxDecoration(
+                         borderRadius: BorderRadius.circular(8.0),
+                        image:const DecorationImage(
+                          image:NetworkImage('https://i.pinimg.com/474x/92/3a/79/923a79f423de552c2057db61d3d88450.jpg'),
+                          fit: BoxFit.cover )
+                      )
+                      ),
+                    Container(
+                      margin:const EdgeInsets.all(2),
+                      decoration:BoxDecoration( 
+                         borderRadius: BorderRadius.circular(8.0),
+                        image:const DecorationImage(
+                          image:NetworkImage('https://i.pinimg.com/474x/56/78/9c/56789cec8a420e48c8203cc2d13c2027.jpg'),
+                          fit: BoxFit.cover
+                        ))
+                      ),
+                      Container(
+                        margin:const EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                           borderRadius: BorderRadius.circular(8.0),
+                           image:const DecorationImage(
+                            image:NetworkImage('https://i.pinimg.com/474x/e8/9b/09/e89b096495bbc18aa6ee59ca3174e8e9.jpg'),
+                            fit: BoxFit.cover
+                           ))
+                        ),
+                         Container(
+                        margin:const EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                           borderRadius: BorderRadius.circular(8.0),
+                           image:const DecorationImage(
+                            image:NetworkImage('https://i.pinimg.com/474x/cb/5a/cf/cb5acfaf184a131bc125e0820932fc85.jpg'),
+                            fit: BoxFit.cover
+                           ))
+                        ),
+                         Container(
+                         
+                        margin:const EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                           borderRadius: BorderRadius.circular(8.0),
+                           image:const DecorationImage(
+                            image:NetworkImage('https://i.pinimg.com/474x/17/92/bd/1792bd231e0ef94863b3394c672a8096.jpg'),
+                            fit: BoxFit.cover
+                           ))
+                        ),
 
-              ),
+                ], options: 
+                CarouselOptions(
+                 
+                  height:400,
+                  autoPlay:true,
+                  enlargeCenterPage: true,
+                  aspectRatio: 16/9,
+                  enableInfiniteScroll: true,
+
+                ))
+              ]
+               ),
               drawer:Drawer(
                 child:ListView( children: const <Widget>[
                   DrawerHeader(
